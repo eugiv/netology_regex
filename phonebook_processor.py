8007031858a24473d97fbdae7ea6153fdb0b5ee8
eugiv <eugene.ivanov@ymail.com>
2023-12-15 02:56:14 +0300

    update

--- phonebook_processor.py
+++ phonebook_processor.py
@@ -1,7 +1,14 @@
+import re
 import csv
 from pprint import pprint
 
 with open('phonebook_raw.csv') as f:
     rows = csv.reader(f, delimiter=',')
     contact_list = list(rows)
-pprint(contact_list)
+
+for lst in contact_list:
+    for cnt in range(4):
+        name = re.findall(r"\w+", str(lst))[cnt]
+    pprint(name)
+
+    email = re.findall(r"[a-zA-Z0-9.-]+@[a-zA-Z.-]+\.(?:ru|com|gov|org|net)", str(lst))
