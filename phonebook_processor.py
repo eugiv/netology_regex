fdbb2cac4674ee31a4e41634266fe6fadab84892
eugiv <eugene.ivanov@ymail.com>
2023-12-17 00:36:04 +0300

    phone pattern

--- phonebook_processor.py
+++ phonebook_processor.py
@@ -6,20 +6,24 @@ with open('phonebook_raw.csv') as f:
     rows = csv.reader(f, delimiter=',')
     contact_list = list(rows)
 
+phone_pattern = r"(\+7|8)?[\s-]?\(?(\d{3})\)?[\s-]?(\d{3})[\s-]?(\d{2})[\s-]?(\d{2})\s?\(?(доб\.?)?\s?(\d+)?\)?"
+phone_substitute = r"+7(\2)\3-\4-\5 \6\7"
+
+
+def phone_format(pat, sub, col_ind=5):
+    phone_new = re.sub(pat, sub, str(lst[col_ind]))
+    return phone_new
+
+
 new_contact_list = []
 for lst in contact_list[1:]:
     fio_pos = re.findall(r"[а-яА-Я]+", str(lst[0:4]))
     for i in range(0, len(fio_pos)):
         lastname = re.findall(r"[а-яА-Я]+", str(lst))[i]
-        # pprint(lastname)
     position = re.findall(r".+", str(lst[4]))
-    # phone = re.findall(r"(?:\+7\s*|8\s*)?\(*\s*\d+\)*\s*\d+[-\s]*\d+[-\s]*\d+"
-    #                    r"[-\s]*\(*[\w.]*\s*\d+\)*", str(lst))
-    phone = re.findall(r"(?:\+7\s*|8\s*)?\(*\s*(\d+)\)*\s*(\d+)[-\s]*(\d+)[-\s]*(\d+)"
-                       r"[-\s]*\(*[\w.]*\s*(\d+)\)*", str(lst))
-
+    phone = phone_format(phone_pattern, phone_substitute)
     email = re.findall(r"(?:\w+|\d+|\.|-)@\w+\.\w+", str(lst))
-    pprint(phone)
+    pprint(email)
 
     # --alternatives--
     # surname = re.findall(r"\w+(?:вич|вна)", str(lst))
